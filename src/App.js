a9ca9b5bf145789f40ceffabce18b4ff6723f0f9
adityaprimafelicitas <aditya@primafelicitas.com>
2024-11-05 14:26:43 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Home from "./components/home";
-import { useEffect, useState } from "react";
-import { collection, getDocs } from "firebase/firestore";
-import { db } from "./firebase";
 
 function App() {
   return (
